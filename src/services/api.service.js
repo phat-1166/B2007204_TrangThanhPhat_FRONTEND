c48a8edd24f161e3a57f7cc274ecc8a0cd49efde
phat16 <trangphat16.cantho@gmail.com>
2023-04-05 20:42:12 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/services/api.service.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+const commonConfig = {
+	headers: { "Content-Type": "application/json", Accept: "application/json" },
+};
+export default (baseURL) => {
+	return axios.create({ baseURL, ...commonConfig });
+};
